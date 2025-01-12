12b94f1b300bc7bd41bf42afb432e5f89adf717d
dereje-boy <Derejeg35@gmail.com>
2025-01-12 14:24:48 +0300

    initial setup

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req,res)=>{
+	res.send("Welcome to guje clinic");
+});
+
+
+app.listen(3000, ()=>console.log("The server started on port 3000"))
